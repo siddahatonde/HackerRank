d5ad8a50d4052e76d6784d3911513a41da7dffb8
Siddhesh Dahatonde <60486266+siddahatonde@users.noreply.github.com>
2021-07-26 01:01:10 +0530

    Create solution.java

---
+++ Java/solution.java
@@ -0,0 +1,17 @@
+/*
+Hackerrank Java End-of-file Solution
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);  
+        int i = 0;
+        while (sc.hasNextLine()) {
+            System.out.println(++i + " " + sc.nextLine());
+        }
+    }
+}
